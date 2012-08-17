337bb71b3e535f3334e21d5299dc1d5cd8ca7c90
carlogan2010 <carlogan2010@gmail.com>
2012-08-17 21:55:41 +0000

    ejemplo de SwingX

---
+++ SIGEVI/src/sigevi/uti/CompletionService.java
@@ -0,0 +1,17 @@
+package sigevi.uti;
+
+public interface CompletionService<T> {
+
+    /**
+     * Autocomplete the passed string. The method will return the matching
+     * object when one single object matches the search criteria. As long as
+     * multiple objects stored in the service matches, the method will return
+     * <code>null</code>.
+     * 
+     * @param startsWith
+     *            prefix string
+     * @return the matching object or <code>null</code> if multiple matches are
+     *         found.
+     */
+    T autoComplete(String startsWith);
+}
