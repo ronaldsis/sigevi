0caacf4ff4b61b484c6f4dbe4e554e3b1cc27336
carlogan2010@gmail.com <carlogan2010@gmail.com@390f7ae1-d57d-2726-3364-ac663e15487d>
2012-09-06 09:56:36 +0000

    Clases para manejo de tabla dinamica 2

--- trunk/SIGEVI/src/sigevi/gui/NewJFrame.java
+++ trunk/SIGEVI/src/sigevi/gui/NewJFrame.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package sigevi.gui;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
@@ -27,45 +23,39 @@ import sigevi.uti.TableService;
  */
 public class NewJFrame extends javax.swing.JFrame {
 
-                   JComboBox categorias = new JComboBox(cargarProductos());
+    
+
     public NewJFrame() {
         initComponents();
-AutoCompleteDecorator.decorate(categorias);
-         MyTable();
+       AutoCompleteDecorator.decorate(jComboBox1); 
+        MyTable();
     }
-    
-    private void MyTable() {
 
-        tabla.setRowHeight(30);
-
-       
-        
-        String[] columnNames = new String[]{ "Nombre", "CANTIDAD", "Fumador", "Nacionalidad", "" };
+    public void MyTable() {
+        JComboBox categorias = new JComboBox(cargarProductos());
+        AutoCompleteDecorator.decorate(categorias);
+        String[] columnNames = new String[]{"Nombre", "CANTIDAD", "Fumador", "Nacionalidad", ""};
         Object[][] data = new Object[][]{
-            {"PIEDRA", "", false, "Click para elegir", new JButton("Reset")},
-            {"PAPEL", "", false, "Click para elegir", new JButton("Reset")},
-            {"TIJERA", "", false, "Click para elegir", new JButton("Reset")}
-        };
+                                        {"PIEDRA", "", false, "", new JButton("Reset")},
+                                        {"PAPEL", "", false, "", new JButton("Reset")},
+                                        {"TIJERA", "", false, "", new JButton("Reset")}
+                                        };
 
-        TableService model = new TableService(columnNames, data) {
-        };
-
+        TableService model = new TableService(columnNames, data) {};
         // Establecemos el modelo
         tabla.setModel(model);
-
         // Establecemos el renderer y editor que usaremos para el boton
         tabla.setDefaultRenderer(JButton.class, new ButtonService());
         tabla.setDefaultEditor(JButton.class, new ButtonService());
-
         // Editores para cada tipo de objeto, estos nos permitirán darles el comportamiento adecuado
         tabla.getColumn("Nacionalidad").setCellEditor(new DefaultCellEditor(categorias));
         tabla.setDefaultEditor(JCheckBox.class, new DefaultCellEditor(new JCheckBox()));
     }
-  
-        private Vector cargarProductos() {
+
+    private Vector cargarProductos() {
         SqlMapClient sqlMapClient = SqlMapConfig.getSqlMap();
         List<Producto> categorias = new ArrayList<>();
-        Vector vector=new Vector();
+        Vector vector = new Vector();
         try {
             categorias = sqlMapClient.queryForList("listProducto", null);
         } catch (SQLException ex) {
@@ -74,6 +64,7 @@ AutoCompleteDecorator.decorate(categorias);
 
         for (int i = 0; i < categorias.size(); i++) {
             Producto per = categorias.get(i);
+            jComboBox1.addItem(per.getNomPro());
             vector.add(per.getNomPro());
         }
         return vector;
@@ -84,7 +75,6 @@ AutoCompleteDecorator.decorate(categorias);
 
     }
 
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -92,8 +82,7 @@ AutoCompleteDecorator.decorate(categorias);
         jScrollPane2 = new javax.swing.JScrollPane();
         tabla = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
-        jScrollPane3 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jComboBox1 = new javax.swing.JComboBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -127,53 +116,29 @@ AutoCompleteDecorator.decorate(categorias);
             }
         });
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ) {
-            Class[] types = new Class [] {
-                java.lang.Boolean.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class
-            };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
-        });
-        jScrollPane3.setViewportView(jTable1);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(68, 68, 68)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 663, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(87, 87, 87)
-                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(31, Short.MAX_VALUE))
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 577, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(164, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jButton1)
-                .addGap(8, 8, 8)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(279, 279, 279))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
 
         pack();
@@ -182,7 +147,7 @@ AutoCompleteDecorator.decorate(categorias);
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         int fila = this.tabla.getSelectedRow();
         String dato = String.valueOf(this.tabla.getValueAt(fila, 1));
-        System.out.println("dato:::::::::::"+dato);
+        System.out.println("dato:::::::::::" + dato);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -221,9 +186,8 @@ AutoCompleteDecorator.decorate(categorias);
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JScrollPane jScrollPane3;
-    private javax.swing.JTable jTable1;
     private javax.swing.JTable tabla;
     // End of variables declaration//GEN-END:variables
 }
