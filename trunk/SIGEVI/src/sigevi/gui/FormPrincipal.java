71b152cf203f078b9273c56f40044705b3cb012b
carlogan2010@gmail.com <carlogan2010@gmail.com@390f7ae1-d57d-2726-3364-ac663e15487d>
2012-09-06 09:05:26 +0000

    Clases para manejo de tabla dinamica 2

--- trunk/SIGEVI/src/sigevi/gui/FormPrincipal.java
+++ trunk/SIGEVI/src/sigevi/gui/FormPrincipal.java
@@ -20,9 +20,9 @@ public class FormPrincipal extends javax.swing.JFrame {
         super();
 
         try {
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
             //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+           // UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
             //UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");
             //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
